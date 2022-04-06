c552e6d27f20bef7746420f1e097165c3ce131f3
migue <miguel.boettcher@hotmail.com>
2022-04-06 20:08:46 +0200

    feat: Adding a column with the municipality key in AGS-format and determining the number of accidents in 2020

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import glob
 
+#Einlesen der Daten und Verknüpfung in einem DataFrame.
 files = glob.glob('data/*.csv')
 
 df_unfallatlas = pd.DataFrame()
 for f in files:
-    csv = pd.read_csv(f, delimiter= ';', nrows = 10)
+    csv = pd.read_csv(f, delimiter= ';', low_memory = False)
     df_unfallatlas = pd.concat([df_unfallatlas, csv])
 
 
+#Aufbereitung der Daten.
 df_unfallatlas['ULICHTVERH'] = df_unfallatlas['ULICHTVERH'].fillna(df_unfallatlas['LICHT'])
 df_unfallatlas['STRZUSTAND'] = df_unfallatlas['STRZUSTAND'].fillna(df_unfallatlas['IstStrasse'])
 df_unfallatlas['IstSonstige'] = df_unfallatlas['IstSonstige'].fillna(df_unfallatlas['IstSonstig'])
@@ -16,4 +18,16 @@ df_unfallatlas['IstSonstige'] = df_unfallatlas['IstSonstige'].fillna(df_unfallat
 df_unfallatlas.drop(['FID', 'OBJECTID', 'OBJECTID_1', 'LICHT', 'IstStrasse', 'IstSonstig'], axis = 1, inplace = True)
 
 df_unfallatlas = df_unfallatlas.iloc[:, [0,1,2,3,4,5,6,7,8,9,10,11,22,12,13,14,15,16,23,17,18,19,20,21,24]]
+
+df_unfallatlas['key'] = df_unfallatlas['ULAND'].astype(str).str.zfill(2) \
+                      + df_unfallatlas['UREGBEZ'].astype(str).str.zfill(1) \
+                      + df_unfallatlas['UKREIS'].astype(str).str.zfill(2) \
+                      + df_unfallatlas['UGEMEINDE'].astype(str).str.zfill(3)
+
+
+#Ausgabe der Unfälle in Siegen im Jahr 2020.
+df_unfallatlas_year = df_unfallatlas.loc[df_unfallatlas['UJAHR'] == 2020]
+print('Anzahl der Unfälle in Siegen:', len(df_unfallatlas_year.loc[df_unfallatlas_year['key'] == '05970040']))
+
+
 
