cf5c46a4e9e8a809c83b5007674f5b4cf43a0b7f
migue <miguel.boettcher@hotmail.com>
2022-04-06 12:50:56 +0200

    concat data

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import glob
 
-df_2016 = pd.read_csv('data/Unfallorte2016_LinRef.csv', delimiter= ';')
-df_2017 = pd.read_csv('data/Unfallorte2017_LinRef.csv', delimiter= ';')
-df_2018 = pd.read_csv('data/Unfallorte2018_LinRef.csv', delimiter= ';')
-df_2019 = pd.read_csv('data/Unfallorte2019_LinRef.csv', delimiter= ';')
-df_2020 = pd.read_csv('data/Unfallorte2020_LinRef.csv', delimiter= ';')
+files = glob.glob('data/*.csv')
 
-print(df_2016)
+df_unfallatlas = pd.DataFrame()
+for f in files:
+    csv = pd.read_csv(f, delimiter= ';', nrows = 10)
+    df_unfallatlas = pd.concat([df_unfallatlas, csv])
+
+
+df_unfallatlas['ULICHTVERH'] = df_unfallatlas['ULICHTVERH'].fillna(df_unfallatlas['LICHT'])
+df_unfallatlas['STRZUSTAND'] = df_unfallatlas['STRZUSTAND'].fillna(df_unfallatlas['IstStrasse'])
+df_unfallatlas['IstSonstige'] = df_unfallatlas['IstSonstige'].fillna(df_unfallatlas['IstSonstig'])
+
+df_unfallatlas.drop(['FID', 'OBJECTID', 'OBJECTID_1', 'LICHT', 'IstStrasse', 'IstSonstig'], axis = 1, inplace = True)
+
+df_unfallatlas = df_unfallatlas.iloc[:, [0,1,2,3,4,5,6,7,8,9,10,11,22,12,13,14,15,16,23,17,18,19,20,21,24]]
+
