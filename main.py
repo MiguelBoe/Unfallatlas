f7a1f926e04c683f1898a4b7363c19cf7786aa7b
migue <miguel.boettcher@hotmail.com>
2022-04-26 19:51:21 +0200

    Data-Preprocessing, Refactoring & Features refactor: Delete some Plots which are not more necessary. feat: New Features are a prediction of the accident severity and a function which shows a map with the locations of chosen accidents. The accidents can be chosen with a query.

--- main.py
+++ main.py
@@ -5,26 +5,30 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+import folium
+from folium import plugins
+import webbrowser
 
-# Einlesen der Daten und Verknüpfung in einem DataFrame.________________________________________________________________
+# Einlesen der Daten und Verknüpfung in einem DataFrame._____________________________________________________________
 
 
 # Einlesen der Daten.
 files = glob.glob('data/*.csv')
 df_unfallatlas = pd.DataFrame()
 for f in files:
-    csv = pd.read_csv(f, delimiter=';', low_memory = False)
+    csv = pd.read_csv(f, delimiter=';', low_memory=False)
     df_unfallatlas = pd.concat([df_unfallatlas, csv])
 
 # Informationen über den Datensatz.
-df_unfallatlas.info()
-df_description = df_unfallatlas.describe()
+# df_unfallatlas.info()
+# df_description = df_unfallatlas.describe()
 
-# Aufbereitung der Daten._______________________________________________________________________________________________
+
+# Aufbereitung der Daten._______________________________________________________________________________________________________
 
 
 # Aktualisierung des Index.
-df_unfallatlas = df_unfallatlas.reset_index(drop = True)
+df_unfallatlas = df_unfallatlas.reset_index(drop=True)
 
 # Zusammenführen der Spalten.
 df_unfallatlas['ULICHTVERH'] = df_unfallatlas['ULICHTVERH'].fillna(df_unfallatlas['LICHT'])
@@ -33,7 +37,7 @@ df_unfallatlas['IstSonstige'] = df_unfallatlas['IstSonstige'].fillna(df_unfallat
 
 # Entfernen der überflüssigen Attribute.
 df_unfallatlas.drop(['FID', 'OBJECTID', 'OBJECTID_1', 'LICHT', 'IstStrasse', 'IstSonstig',
-                     'UIDENTSTLA', 'UIDENTSTLAE', 'LINREFX', 'LINREFY'], axis = 1, inplace = True)
+                     'UIDENTSTLA', 'UIDENTSTLAE', 'LINREFX', 'LINREFY'], axis=1, inplace=True)
 
 # Erstellung des AGS.
 df_unfallatlas['AGS'] = df_unfallatlas['ULAND'].astype(str).str.zfill(2) \
@@ -42,8 +46,8 @@ df_unfallatlas['AGS'] = df_unfallatlas['ULAND'].astype(str).str.zfill(2) \
                         + df_unfallatlas['UGEMEINDE'].astype(str).str.zfill(3)
 
 # Umbenennung der Spalten YGCSWGS84 und XGCSWGS84.
-df_unfallatlas.rename(columns = {"YGCSWGS84": "lat"}, inplace=True)
-df_unfallatlas.rename(columns = {"XGCSWGS84": "lon"}, inplace=True)
+df_unfallatlas.rename(columns={"YGCSWGS84": "lat"}, inplace=True)
+df_unfallatlas.rename(columns={"XGCSWGS84": "lon"}, inplace=True)
 
 # Konvertierung der Standortspalten von europäischen Komma-Sep-Werten in punktgetrennte Werte.
 df_unfallatlas["lon"] = df_unfallatlas["lon"].apply(lambda a: a.replace(",", "."))
@@ -59,29 +63,19 @@ df_unfallatlas.insert(21, 'lon', df_unfallatlas.pop('lon'))
 df_unfallatlas.insert(21, 'lat', df_unfallatlas.pop('lat'))
 df_unfallatlas.insert(13, 'STRZUSTAND', df_unfallatlas.pop('STRZUSTAND'))
 
-# Prediction IstGkfz.___________________________________________________________________________________________________
+# Prediction IstGkfz._____________________________________________________________________________________________________________________
 
-
-# Darstellung fehlender Werte im Datensatz.
-sns.set()
-fig, ax = plt.subplots(figsize=(15, 10))
-sns.heatmap(df_unfallatlas.isnull(), yticklabels=False, cbar=False, cmap='viridis')
-plt.title('Fehlende Werte im Datensatz (in gelb)', fontsize=30, pad=20)
-plt.ylabel('Datenpunkte', fontsize=25)
-plt.xlabel('AGS', fontsize=25)
-ax.tick_params(axis='x', labelsize=20)
-ax.tick_params(axis='y', labelsize=20)
-plt.show()
 
 # Erstellung eines Datensatzes zur Abschätzung der fehlenden IstGkfz für 2017.
+df_unfallatlas.loc[df_unfallatlas.IstSonstige == 0, 'IstGkfz'] = 0
 df_unfallatlas_istGkfz = df_unfallatlas[df_unfallatlas['IstGkfz'].notnull()]
 
-# Define x and y.
+# Definition von X und y.
 X = df_unfallatlas_istGkfz.drop(['IstGkfz'], axis=1)
 y = df_unfallatlas_istGkfz['IstGkfz']
 
 # Splitten der Daten in Test- und Training-Set.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
 # Training der Daten.
 decision_tree_classification = DecisionTreeClassifier().fit(X_train, y_train)
@@ -95,33 +89,73 @@ print('\nAccuracy-Score des Modells:', round(score, 2))
 
 # Validierung der Predictions in einem DataFrame
 results['IstGkfz'] = df_unfallatlas['IstGkfz']
+results.rename(columns={0: "pred"}, inplace=True)
+results.rename(columns={"IstGkfz": "test"}, inplace=True)
 
 # Abschätzung der fehlenden IstGkfz-Werte für das Jahr 2017 mit dem Decision Tree Klassifikationsmodell.
 df_unfallatlas_pred_gkfz = df_unfallatlas[df_unfallatlas['IstGkfz'].isnull()]
 df_unfallatlas_pred_gkfz = df_unfallatlas_pred_gkfz.drop('IstGkfz', axis=1)
-istGkfz_2017 = pd.DataFrame(decision_tree_classification.predict(df_unfallatlas_pred_gkfz),
-                            index=df_unfallatlas_pred_gkfz.index)
+df_IstGkfz_2017 = pd.DataFrame(decision_tree_classification.predict(df_unfallatlas_pred_gkfz),
+                               index=df_unfallatlas_pred_gkfz.index)
 
 # Ergänzung der fehlenden IstGkfz-Werte im DataFrame df_unfallatlas.
-df_unfallatlas['IstGkfz'] = df_unfallatlas['IstGkfz'].fillna(istGkfz_2017[0])
+df_unfallatlas['IstGkfz'] = df_unfallatlas['IstGkfz'].fillna(df_IstGkfz_2017[0])
+df_unfallatlas.loc[df_unfallatlas.IstSonstige == 0, 'IstGkfz'] = 0
+df_unfallatlas.loc[(df_unfallatlas['UJAHR'] == 2017) & (df_unfallatlas['IstSonstige'] == 1) & (
+            df_unfallatlas['IstGkfz'] == 1), 'IstSonstige'] = 0
 
-# Analyse der Daten.____________________________________________________________________________________________________
+# Analyse._____________________________________________________________________________________________________________________
 
 
-# Ermittlung der Anzahl der Unfälle in den einzelnen Gemeinden in den Jahren von 2016 - 2020.
-df_unfallatlas_count = df_unfallatlas.groupby('AGS').size().reset_index(name = 'number_of_accidents')
-df_unfallatlas_count = df_unfallatlas_count.sort_values('number_of_accidents', ascending=False)
+# Abfrage.
+ags = input('\nGemeinde: ')
+# year = int(input('Jahr: '))
+category = int(input('Kategorie: '))
+# monat = int(input('Monat: '))
 
-# Darstellung der 10 Gemeinden mit den meisten Unfällen in den Jahren von 2016 - 2020.
-sns.set()
-fig, ax = plt.subplots(figsize=(15, 10))
-sns.barplot(x='AGS', y='number_of_accidents', data=df_unfallatlas_count.head(10))
-plt.title('Die 10 Gemeinden mit den meisten Unfällen von 2016 - 2020', fontsize=30, pad=20)
-plt.ylabel('Anzahl der Unfälle', fontsize=25)
-plt.xlabel('AGS', fontsize=25)
-ax.tick_params(axis='x', labelsize=15)
-ax.tick_params(axis='y', labelsize=20)
-plt.show()
+df_unfallatlas_query = df_unfallatlas[
+    # (df_unfallatlas['UJAHR'] == year) &
+    (df_unfallatlas['AGS'] == ags) &
+    (df_unfallatlas['UKATEGORIE'] == category)
+    # & (df_unfallatlas['UMONAT'] == category)
+    ]
+
+df_unfallatlas_query = df_unfallatlas_query.reset_index(drop=True)
+
+
+# Darstellung der Unfallorte.
+class Map:
+    def __init__(self, center, zoom_start, locationlist, locations):
+        self.center = center
+        self.zoom_start = zoom_start
+        self.locationlist = locationlist
+
+    def showMap(self):
+        # Create the map
+        my_map = folium.Map(location=self.center, zoom_start=self.zoom_start)
+        for point in range(0, len(locationlist)):
+            folium.CircleMarker(locationlist[point], popup=df_unfallatlas_query['UART'][point], radius=3).add_to(my_map)
+        my_map.add_child(plugins.HeatMap(locations, radius=15))
+        # Display the map
+        my_map.save("map.html")
+        webbrowser.open("map.html")
+
+
+# Define coordinates of where we want to center our map
+locations = df_unfallatlas_query[['lat', 'lon']]
+locationlist = locations.values.tolist()
+coords = [df_unfallatlas_query['lat'].mean(), df_unfallatlas_query['lon'].mean()]
+
+map = Map(center=coords, zoom_start=12, locationlist=locationlist, locations=locations.values)
+map.showMap()
+
+# 05570020
+# 05970040
+# 05315000
+
+
+# Predict Unfalltyp._____________________________________________________________________________________________________________________
+
 
 # Darstellung der Korrelation der Attribute
 plt.figure(figsize=(15, 10))
@@ -129,17 +163,19 @@ plt.title('Korrelation der Attribute', fontsize=25, pad=20)
 sns.heatmap(df_unfallatlas.corr(), annot=True, robust=True)
 plt.show()
 
-# Abfragen._____________________________________________________________________________________________________________
+# Definition von X und y.
+X = df_unfallatlas.drop(['UTYP1', 'lat', 'lon'], axis=1)
+y = df_unfallatlas['UTYP1']
 
+# Splitten der Daten in Test- und Training-Set.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-# Ausgabe der Unfälle in Siegen im Jahr 2020.
-df_unfallatlas_2020 = df_unfallatlas.loc[df_unfallatlas['UJAHR'] == 2020]
-print('\nAnzahl der Unfälle in Siegen:', len(df_unfallatlas_2020.loc[df_unfallatlas_2020['AGS'] == '05970040']))
+# Training der Daten.
+decision_tree_classification = DecisionTreeClassifier(max_depth=10, random_state=1).fit(X_train, y_train)
 
-# Abfrage der Anzahl der Unfälle nach Jahr und AGS.
-jahr = int(input('\nJahr: '))
-ags = input('Gemeinde: ')
-df_unfallatlas_year = df_unfallatlas.loc[df_unfallatlas['UJAHR'] == jahr]
-print(f'\nAnzahl der Unfälle in {ags}:', len(df_unfallatlas_year.loc[df_unfallatlas_year['AGS'] == ags]))
+# Validierung des Modells.
+results = pd.DataFrame(decision_tree_classification.predict(X_test), index=X_test.index)
 
-
+# Überprüfung der Genauigkeit des Modells.
+score = accuracy_score(y_test, results)
+print('\nAccuracy-Score des Modells:', round(score, 2))
