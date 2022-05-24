74ac39257616dd02b8aa20a345421a29979512d9
migue <miguel.boettcher@hotmail.com>
2022-05-24 15:27:26 +0200

    Features feat: Add class_weights for the models.

--- model.py
+++ model.py
@@ -100,7 +100,7 @@ def visualization_ts(df_number_of_accidents, prediction):
 
 #Vorhersage der schwere des Unfalls.____________________________________________________________________________________
 
-#DecisionTree mit RandomUndersampling als Baseline Modell.
+#DecisionTree mit RandomUndersampling und ohne Klassengewichte als Baseline Modell.
 def pred_accident_severity_decision_tree(df_unfallatlas, adjusted_score, undersampling_mode):
 
     #Definition von X und y.
@@ -114,8 +114,8 @@ def pred_accident_severity_decision_tree(df_unfallatlas, adjusted_score, undersa
     X_train, y_train = undersampling(X_train, y_train, undersampling_mode)
 
     #Training der Daten.
-    decision_tree_model = DecisionTreeClassifier(max_depth = 10).fit(X_train, y_train)
-    #joblib.dump(decision_tree_model, 'models/decision_tree_model.sav')
+    decision_tree_model = DecisionTreeClassifier(max_depth = 10, class_weight={1:0.40, 2:0.40, 3:0.20}).fit(X_train, y_train)
+    joblib.dump(decision_tree_model, 'models/decision_tree_model.sav')
 
     #Validierung des Modells.
     results = pd.DataFrame(decision_tree_model.predict(X_test), index = X_test.index)
@@ -144,8 +144,8 @@ def pred_accident_severity_random_forest(df_unfallatlas, adjusted_score, undersa
     X_train, y_train = undersampling(X_train, y_train, undersampling_mode)
 
     #Training der Daten.
-    random_forest_model = RandomForestClassifier(max_depth = 10).fit(X_train, y_train)
-    #joblib.dump(random_forest_model, 'models/random_forest_model.sav')
+    random_forest_model = RandomForestClassifier(max_depth = 10, class_weight={1:0.40, 2:0.40, 3:0.20}).fit(X_train, y_train)
+    joblib.dump(random_forest_model, 'models/random_forest_model.sav')
 
     #Validierung des Modells.
     results = pd.DataFrame(random_forest_model.predict(X_test), index = X_test.index)
@@ -174,7 +174,7 @@ def pred_accident_severity_gaussian_nb(df_unfallatlas, adjusted_score, undersamp
     X_train, y_train = undersampling(X_train, y_train, undersampling_mode)
 
     #Training der Daten.
-    gaussian_nb_model = GaussianNB().fit(X_train, y_train)
+    gaussian_nb_model = GaussianNB(class_weight={1:0.40, 2:0.40, 3:0.20}).fit(X_train, y_train)
     joblib.dump(gaussian_nb_model, 'models/gaussian_nb_model.sav')
 
     #Validierung des Modells.
@@ -208,7 +208,7 @@ def pred_accident_severity_svm(df_unfallatlas, adjusted_score, undersampling_mod
     X_train, y_train = undersampling(X_train, y_train, undersampling_mode)
 
     # Training der Daten.
-    svm_model = svm.SVC(kernel='linear').fit(X_train, y_train)
+    svm_model = svm.SVC(kernel='poly', class_weight={1:0.40, 2:0.40, 3:0.20}).fit(X_train, y_train)
     joblib.dump(svm_model, 'models/svm_model.sav')
 
     # Validierung des Modells.
@@ -233,6 +233,9 @@ def pred_accident_severity_nearest_neighbors(df_unfallatlas, adjusted_score, und
 
     #Splitten der Daten in Test- und Training-Set.
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10)
+
+    # Undersampling.
+    X_train, y_train = undersampling(X_train, y_train, undersampling_mode)
 
     #Training der Daten.
     knn_model = KNeighborsClassifier(n_neighbors = 3).fit(X_train, y_train)
